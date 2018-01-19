1356871970dd3a31a595cc47be58ad7377d4b633
Diana <diana.sinenchenko@gmail.com>
2018-01-19 10:08:23 +0200

    add signup and activate

--- users_auth2/authentication/views.py
+++ users_auth2/authentication/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users_auth2.authentication.forms import SignUpForm
+from users_auth2.authentication.tokens import account_activation_token
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_text
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = True
+            user.save()
+            request.session['uidb64'] = user.pk
+
+            return redirect('user_page')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def account_activation_sent(request):
+    return render(request, 'account_activation_sent.html')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user)
+
+       # return redirect('')
+    else:
+        return render(request, 'account_activation_invalid.html')
+
+
