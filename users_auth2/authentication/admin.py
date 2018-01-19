6595bf1c406260af47e810c4f4e660b12b7bb7ff
Diana <diana.sinenchenko@gmail.com>
2018-01-19 09:37:51 +0200

    registration Profile models

--- users_auth2/authentication/admin.py
+++ users_auth2/authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from  users_auth.authentication.models import Profile
 # Register your models here.
+#admin.site.register(Profile)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+    fields = ['user', 'bio']
