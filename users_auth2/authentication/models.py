52e27f13cc6a4082242bfd0835067629fc9e7c36
Diana <diana.sinenchenko@gmail.com>
2018-01-19 09:33:56 +0200

    create models Profile

--- users_auth2/authentication/models.py
+++ users_auth2/authentication/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    email_confirmed = models.BooleanField(default=False)
+    password = models.CharField(max_length=100, null=True, default=None)
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+
+def __str__(self):
+     return '{0}, {1}'.format(self.user, self.bio)
