910adcdcc5c4f8f4554a06cab0668413a3305a02
Diana <diana.sinenchenko@gmail.com>
2018-01-19 10:16:11 +0200

    add home and user_page

--- users_auth2/authentication/views.py
+++ users_auth2/authentication/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from users_auth2.authentication.forms import SignUpForm
 from users_auth2.authentication.tokens import account_activation_token
-from django.utils.http import urlsafe_base64_decode
-from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
 from django.contrib.auth.models import User
 from django.contrib.auth import login
+
+
+def home(request):
+    return render(request, 'home.html')
 
 
 def signup(request):
@@ -14,6 +18,7 @@ def signup(request):
             user = form.save(commit=False)
             user.is_active = True
             user.save()
+
             request.session['uidb64'] = user.pk
 
             return redirect('user_page')
@@ -43,4 +48,17 @@ def activate(request, uidb64, token):
     else:
         return render(request, 'account_activation_invalid.html')
 
+
+def user_page(request):
+    uidb64 = request.session.get('uidb64')
+    try:
+
+        user = User.objects.get(pk=uidb64)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    return render(
+        request,
+        'user_page.html',
+        context={'user':user}
+    )
 
