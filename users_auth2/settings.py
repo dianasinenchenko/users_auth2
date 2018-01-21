1738024ba63853043458a49cf1f7e2109e793e6b
Diana <diana.sinenchenko@gmail.com>
2018-01-21 20:34:27 +0200

    change LOGIN_REDIRECT_URL to 'home'

--- users_auth2/settings.py
+++ users_auth2/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
-LOGIN_REDIRECT_URL = 'user_page'
+LOGIN_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
